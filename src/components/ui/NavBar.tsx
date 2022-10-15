41c8c6ea8a344bf70dcf5120e8b41b6968a4e1bc
Helcio L. do Carmo Junior <helcio@carmo.in>
2022-10-14 22:36:33 -0300

    feat: Implement index page with navbar and password list

---
+++ src/components/ui/NavBar.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Box, AppBar, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material'
+import { AccountCircle } from '@mui/icons-material'
+
+function NabBar() {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+
+    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget)
+    }
+
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+
+    return (
+        <Box sx={{ display: 'flex', overflow: 'hidden' }}>
+            <AppBar position='sticky' component='nav'>
+                <Toolbar>
+                    <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+                        Password Manager
+                    </Typography>
+                    <div>
+                        <IconButton
+                            size='large'
+                            aria-label='account of current user'
+                            aria-controls='menu-appbar'
+                            aria-haspopup='true'
+                            onClick={handleMenu}
+                            color='inherit'
+                        >
+                            <AccountCircle />
+                        </IconButton>
+                        <Menu
+                            id='menu-appbar'
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorEl)}
+                            onClose={handleClose}
+                        >
+                            <MenuItem onClick={handleClose}>Logout</MenuItem>
+                        </Menu>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
+
+export { NabBar }
