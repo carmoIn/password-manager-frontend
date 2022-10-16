e70629aa1557897310a16ef709c9461658361d90
Helcio L. do Carmo Junior <helcio@carmo.in>
2022-10-16 11:05:44 -0300

    style: Remove unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,6 @@ import { NabBar } from '@/components/ui/NavBar'
 import EnhancedTable from '@/components/ui/DataGrid'
 import { Box, Paper } from '@mui/material'
 import type { NextPage } from 'next'
-import { Password, PasswordCollection } from '@/types/password.types'
-import { useEffect, useState } from 'react'
-import { PasswordClient } from '@/client/password.client'
 
 const Home: NextPage = () => {
     return (
